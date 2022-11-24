31ed61b359bff8faba4cb4abb6c427c02474eac4
Laura Flament <laura.flament6@gmail.com>
2022-11-24 21:34:58 +0100

    Affichage des formulaires inscription et connexion

--- frontend/src/components/Log/LoginForm.js
+++ frontend/src/components/Log/LoginForm.js
@@ -1,10 +1,50 @@
-import React from 'react';
+import React, { useState } from "react";
+import axios from 'axios';
 
 const LoginForm = () => {
+
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+
+const handleLogin = (e) => {
+  e.preventDefault();
+  const emailError = document.querySelector(".email.error");
+  const passwordError = document.querySelector(".password.error");
+
+  axios({
+    method: "post",
+    url: `http://localhost3000/api/auth/login`,
+    withCredentials: true,
+    data: {email, password},
+  })
+  .then((res) => {
+    console.log(res);
+    if (res.data.errors) {
+      emailError.innerHTML = res.data.errors.email;
+      passwordError.innerHTML = res.data.errors.password;
+    } else {
+      window.location = "/";
+    }
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+};
+
   return (
-    <div>
-      Se connecter
-    </div>
+    <form action="" onSubmit={handleLogin} id="sign-up-form">
+      <label>Email</label>
+      <br />
+      <input type="email" name="email" id="email" onChange={(e) => setEmail(e.target.value)} value={email} />
+      <div className="email error"></div>
+      <br />
+      <label>Mot de passe</label>
+      <br />
+      <input type="password" name="password" id="password" onChange={(e) => setPassword(e.target.value)} value={password} />
+      <div className="password error"></div>
+      <br />
+      <input type="submit" value="Se connecter" />
+    </form>
   );
 };
 
--- frontend/src/components/Log/SignUpForm.js
+++ frontend/src/components/Log/SignUpForm.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const SignUpForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div>
-      S'inscrire
-    </div>
+    <form action="" onSubmit={handleRegister} id="sign-up-form">
+      <label>Email</label>
+      <br />
+      <input type="email" name="email" id="email" onChange={(e) => setEmail(e.target.value)} value={email} />
+      <div className="email error"></div>
+      <br />
+      <label>Mot de passe</label>
+      <br />
+      <input type="password" name="password" id="password" onChange={(e) => setPassword(e.target.value)} value={password} />
+      <div className="password error"></div>
+      <br />
+      <input type="submit" value="S'inscrire" />
+    </form>
   );
 };
 
